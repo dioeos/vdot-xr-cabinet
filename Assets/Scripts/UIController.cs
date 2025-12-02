72610251055e9c4a091bbaa6bcadc36886ae3436
Ryan Dioneda <er.dioneda@gmail.com>
2025-12-01 19:26:26 -0500

    Implemented popup display logic (Restructed)

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class PopupUIController : MonoBehaviour {
+
+  [SerializeField]
+  private GameObject plane;
+  public TextMeshPro textField;
+
+  // Start is called once before the first execution of Update after the
+  // MonoBehaviour is created
+  void Start() { Hide(); }
+
+  public void Show(string tmpMessage) {
+    Debug.Log("Showing popup with message: " + tmpMessage);
+    textField.text = tmpMessage;
+    plane.SetActive(true);
+    // canvasGroup.alpha = 1f;            // Make the popup visible
+    // canvasGroup.interactable = true;   // Allow interaction
+    // canvasGroup.blocksRaycasts = true; // Block raycasts to underlying UI
+  }
+
+  public void Hide() {
+    Debug.Log("Hiding popup.");
+    plane.SetActive(false);
+    // canvasGroup.alpha = 0f;
+    // canvasGroup.interactable = false;
+    // canvasGroup.blocksRaycasts = false;
+  }
+
+  void Update() {}
+}
---
+++ Assets/Scripts/UtilFunctions.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class UtilFunctions {
+  public static UnityEngine.XR.Interaction.Toolkit.Interactables
+      .XRGrabInteractable GetXRGrab(Component component) {
+    return component.GetComponent<
+        UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+  }
+}
