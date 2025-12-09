3f1103ee75ecd974d35b886cc44e5240b3379624
Ryan Dioneda <er.dioneda@gmail.com>
2025-12-09 12:52:32 -0500

    Custom drawer funcitonality via XRHandSubsystem data

---
+++ Assets/Scripts/CompositionRoot/CompositionRoot.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CompositionRoot : MonoBehaviour
+{
+  [Header("Services")]
+  [SerializeField]
+  HandTrackingManager handTrackingManager;
+
+  [Header("Consumers")]
+  [SerializeField]
+  DrawerController[] drawers;
+
+  // [SerializeField]
+  // DoorHandleController[] handles;
+
+  void Awake()
+  {
+    foreach (var drawer in drawers)
+    {
+      drawer.Initialize(handTrackingManager);
+    }
+    // foreach (var handle in handles)
+    // {
+    //   handle.Initialize(handTrackingManager);
+    // }
+  }
+}
---
+++ Assets/Scripts/Services/IHandTrackingService.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.XR.Hands;
+
+public enum Handedness { Left, Right }
+
+public interface IHandTrackingService
+{
+  /// tries to get palm, gives Pose of palm if possible
+  bool TryGetPalmPose(Handedness hand, out Pose pose);
+
+  /// tries to get hand
+  bool TryGetHand(Handedness hand, out XRHand xrHand);
+
+  /// determines if hand is "closed"
+  bool IsFist(Handedness hand);
+}
